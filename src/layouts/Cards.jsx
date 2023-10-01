71def3f2913e343813b077a83677786ea2476ce7
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-10-01 18:13:44 +0600

    mini json update

--- src/layouts/Cards.jsx
+++ src/layouts/Cards.jsx
@@ -8,7 +8,7 @@ const Cards = ({ routine }) => {
 				<div className="card-body flex flex-col justify-between">
 					<h2 className="card-title mx-auto">{routine.subject}</h2>
 					<p className="mx-auto text-center">{routine.subjectFull}</p>
-					<p className="mx-auto text-center">{routine.techerFull}</p>
+					<p className="mx-auto text-center">{routine.teacherFull}</p>
 				</div>
 			</div>
 		</>
