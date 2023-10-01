43a2113f2e07c6ca8577880a6c33bfaee88de275
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-10-02 02:00:30 +0600

    minor update 2

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
 						{links}
 					</ul>
 				</div>
-				<a className="btn btn-ghost normal-case text-xl">Routine</a>
+				<a className="btn btn-ghost normal-case text-xl">NEUB</a>
 			</div>
 			<div className="navbar-center hidden lg:flex">
 				<ul className="menu menu-horizontal px-1">
